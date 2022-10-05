f255796547d3e16f81116fd088aec684cfe1866f
Taye-Vaughn Jones <jonestaye@gmail.com>
2022-10-04 20:10:45 -0500

    added X and O to board

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,35 @@
 window.onload = () =>{
     const board = document.getElementById('board')
     const squares = board.querySelectorAll('div')
+    const state = ["","","","","","","","",""]
     
+    
+    //varibale of the next symbol to display
+    let next = 'X'
+
     //set class as square for each div in the game board
     for(const square of squares){
         square.setAttribute('class', 'square')
+        
+        
+        //set X and O
+        square.addEventListener('click',()=>{
+            square.innerHTML = next
+            square.classList.add(next)
+            console.log(squares)
+            state[(Array.from(squares)).indexOf(square)] = next
+            if (next == 'X'){
+                next = 'O'
+            }
+            else{
+                next = 'X'
+            }
+            console.log(state)
+        })
     }
+
+    
+    
 }
 
 
